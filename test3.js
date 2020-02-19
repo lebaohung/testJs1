8a03c7286d4f8b7a145fcc0a957eef745df161ee
Hung <lebaohung.1996@gmail.com>
2020-02-19 15:45:03 +0700

    hoan thanh bai

--- test3.js
+++ test3.js
@@ -1,17 +1,25 @@
-let a = parseInt(prompt("Nhap vao a", "123"));
-let b = parseInt(prompt("Nhap vao b", "123"));
+// let a = parseInt(prompt("Nhap vao a", "123"));
+// le?t b = parseInt(prompt("Nhap vao b", "123"));
+let a;
+let b;
+function inputa() {
+    a = parseInt(prompt("Nhap vao a", "123"));
+}
 
-
-
-// function check(a) {
-//     console.log( a);
-if (a % b === 0)
-{
-    alert("a chia het cho b");
-
-} else
-{
-    alert("a khong chia het cho b");
+function inputb() {
+    b = parseInt(prompt("Nhap vao a", "123"));
 }
 
 
+function check() {
+
+    if (a % b === 0)
+    {
+        alert("a chia het cho b");
+
+    } else
+    {
+        alert("a khong chia het cho b");
+    }
+
+}
